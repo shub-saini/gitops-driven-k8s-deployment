f57bfea0e16b5054e08aed7238f30857be41b656
shub-saini <anshsaini04@gmail.com>
2026-04-01 02:07:14 +0530

    terraform staging workflow added

---
+++ terraform-gcp/envs/staging/provider.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.14"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 7.22.0, < 8.0.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "terraform-staging-state-bucket789" // change this manually after bucket creation
+    prefix = "envs/stage"
+  }
+}
+
+provider "google" {
+  region = var.region
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = module.gke.cluster_endpoint
+  cluster_ca_certificate = module.gke.cluster_ca_certificate
+  token                  = data.google_client_config.default.access_token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.gke.cluster_endpoint
+    cluster_ca_certificate = module.gke.cluster_ca_certificate
+    token                  = data.google_client_config.default.access_token
+  }
+}
